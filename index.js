136efbab1343d1da987cdc9d54056ca8568fbaf0
Diego! <91294213+Kabash87@users.noreply.github.com>
2023-06-25 19:44:45 -0500

    Arreglo del Codigo de Battleship

---
+++ index.js
@@ -0,0 +1,8 @@
+import { setupGame, play } from "./modulos/game.js";
+
+//Battleship game, in Javascript
+//Battleship, programado en Javascript
+//Se prepara el tablero y Opciones del Juego
+setupGame();
+//Se ejecuta el Juego
+play();
